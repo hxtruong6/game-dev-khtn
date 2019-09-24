8ad335a44d84441575d04aea3f28c8fc625bcbe0
hxtruong <hoangxuantruong98@gmail.com>
2019-09-25 01:20:31 +0700

    feat: moving player and enemy

---
+++ SpritesDemo/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] float maxDist;
+    [SerializeField] float minDist;
+    [SerializeField] float movingSpeed;
+    SpriteRenderer _sprite;
+    Animator animator;
+    Vector3 initialPosition;
+    int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        initialPosition = transform.position;
+        direction = -1;
+        //maxDist += transform.position.x;
+        //minDist -= transform.position.x;
+        movingSpeed = 0.3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (direction)
+        {
+            case -1:
+                // Moving Left
+                if (transform.position.x > minDist)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(-movingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+                    _sprite.flipX = false;
+                    animator.SetBool("isMoving", true);
+                }
+                else
+                {
+                    direction = 1;
+                    animator.SetBool("isMoving", false);
+
+                }
+                break;
+            case 1:
+                //Moving Right
+                if (transform.position.x < maxDist)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(movingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+                    _sprite.flipX = true;
+                    animator.SetBool("isMoving", true);
+
+                }
+                else
+                {
+                    direction = -1;
+                    animator.SetBool("isMoving", false);
+
+                }
+                break;
+        }
+    }
+}
---
+++ SpritesDemo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private SpriteRenderer _sprite;
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    private Vector3 movingPos;
+    [SerializeField] private Vector3 stepMoving;
+    private Transform _transform;
+    //[SerializeField] float speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _transform = GetComponent<Transform>();
+        stepMoving = new Vector3(0.05f, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HorizontalMoving())
+        {
+            //    float moveHorizontal = Input.GetAxis("Horizontal");
+            //    print(moveHorizontal);
+            //    Vector2 movement = new Vector2(moveHorizontal, 0);
+
+            _animator.SetBool("isRunning", true);
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.D))
+            {
+                _sprite.flipX = true;
+                movingPos = _transform.position - stepMoving;
+            }
+            else
+            {
+                _sprite.flipX = false;
+                movingPos = _transform.position + stepMoving;
+            }
+            _transform.position = movingPos;
+            //_rb.AddForce(movement * speed * Time.deltaTime);
+
+        }
+        else if (VerticalMoving())
+        {
+            _animator.SetTrigger("isJumping");
+            _rb.AddForce(new Vector2(0, 0.2f), ForceMode2D.Impulse);
+        }
+        else
+        {
+            _animator.SetBool("isRunning", false);
+        }
+    }
+
+    private bool HorizontalMoving()
+    {
+        return Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A);
+    }
+
+    private bool VerticalMoving()
+    {
+        return Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S);
+    }
+}
